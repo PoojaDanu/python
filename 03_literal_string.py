cb8935ac2c338d57cab96303d50e49e64d1e6000
Pooja <lavieinrose111@gmail.com>
2021-11-21 20:47:07 +0530

    Project Intial

---
+++ 03_literal_string.py
@@ -0,0 +1,22 @@
+strings = "This is Python"
+char = "C"
+multiline_str = """This is a multiline string with more than one line code."""
+unicode = u"\u00dcnic\u00f6de"
+raw_str = r"raw \n string"
+
+print(strings)
+print(char)
+print(multiline_str)
+print(unicode)
+print(raw_str)
+#boolean
+x = (1 == False)
+y = (1 == True)
+a = False + 4
+b = True + 10
+
+print("x is", x)
+print("y is", y)
+print("a:", a)
+print("b:", b)
+
---
+++ Code/02_operators.py
@@ -0,0 +1,3 @@
+a=7
+b=14
+print("divison of b and a is", a/b)
